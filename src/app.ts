d304d782cfb1b49a6b0d785f8712faddfbfe7f87
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-13 23:37:40 +0200

    String type

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-// uppercase -> JS
+const coupon: string = 'pizza25';
 
-// lowercase -> TS
-
-const pizzaCost: number = 10;
-const pizzaToppings: number = 2;
-
-function calculatePrice(cost: number, toppings: number): number {
-  return cost + 1.5 * toppings;
+function normalizeCoupon(code: string): string {
+  return code.toUpperCase();
 }
 
-const cost = calculatePrice(pizzaCost, pizzaToppings);
-console.log(`Pizza costs: ${cost}`);
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+
+console.log(couponMessage);
