747d92a20c03b68908cda432afc2f15bb837cf07
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-14 23:26:51 +0200

    Objects

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-let sumOrder: (price: number, quantity?: number) => number;
+let pizza: { name: string; price: number; getName(): string } = {
+  name: 'Plain Old Pepperoni',
+  price: 20,
+  getName() {
+    return pizza.name;
+  },
+};
 
-sumOrder = (x, y = 1) => x * y;
-
-const sum = sumOrder(25);
-sumOrder(25, 2);
-
-console.log(`Total sum: ${sum}`);
+console.log(pizza.getName());
