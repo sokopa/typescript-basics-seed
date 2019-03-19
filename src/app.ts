657392c469e6392e4edea5aac5a5b50424b0f60d
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-19 23:32:42 +0200

    creating interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-type Pizza = { name: string; toppings: number };
-
-const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
-
-const serialized = JSON.stringify(pizza);
-
-console.log(serialized);
-
-function getNameFromJSON(obj: string): string {
-  // return (<Pizza>JSON.parse(obj)).name; // old way
-  return (JSON.parse(obj) as Pizza).name; // preferred way
+interface Pizza {
+  name: string;
+  sizes: string[];
 }
 
-getNameFromJSON(serialized);
+interface Pizzas {
+  data: Pizza[];
+}
+
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes,
+  };
+}
+
+pizza = createPizza('Pepperoni', ['small', 'medium']);
