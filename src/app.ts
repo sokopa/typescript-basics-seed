967d51eb0a44b8b1e750990ca2394523fd026a9d
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-20 23:42:42 +0200

    setters/getters, class inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
-class Pizza {
+class Sizes {
+  constructor(public sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes() {
+    return this.sizes;
+  }
+}
+
+class Pizza extends Sizes {
   toppings: string[] = [];
 
-  constructor(readonly name: string) {}
+  constructor(readonly name: string, public sizes: string[]) {
+    super(sizes);
+  }
 
   addTopping(topping: string) {
     this.toppings.push(topping);
   }
 }
 
-const pizza = new Pizza('Pepperoni');
-pizza.addTopping('pepperoni');
+// element.className = 'abc'; // setter in action
+// console.log(element.className); // getter in action
 
-// pizza.name = 'x'; // not applicable
-console.log(pizza.name);
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+console.log(pizza.availableSizes);
+pizza.addTopping('pepperoni');
