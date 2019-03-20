99d064201f54eed318d1d69802c08b34c2da4e2a
Sotiris Panopoulos <sokopa@gmail.com>
2019-03-20 23:25:07 +0200

    private / public

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 class Pizza {
-  name: string;
   toppings: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
-  }
+  constructor(private name: string) {} // shorthand to also declare private property
 
+  // by default, all are public
   addTopping(topping: string) {
     this.toppings.push(topping);
   }
 }
 
 const pizza = new Pizza('Pepperoni');
-
 pizza.addTopping('pepperoni');
 
 console.log(pizza);
